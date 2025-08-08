83e3ea19303de3231ff8f3f1ea586f940171f8bb
Wojciech Trybus <wojtryb@gmail.com>
2025-08-08 16:57:50 +0200

    Add "Remove Hidden Layers"  action

--- wojtryb_krita_helpers/wojtryb_krita_helpers.py
+++ wojtryb_krita_helpers/wojtryb_krita_helpers.py
@@ -3,7 +3,7 @@
 
 from typing import Callable
 
-from krita import Krita, Extension, Window
+from krita import Krita, Extension, Window, GroupLayer
 
 
 class WojtrybKritaHelpers(Extension):
@@ -25,10 +25,39 @@ class WojtrybKritaHelpers(Extension):
         original.parentNode().addChildNode(duplicate, original)
 
         # Collapse and hide the original, show the duplicate
-        if original.type() == "grouplayer":
+        if isinstance(original, GroupLayer):
             original.setCollapsed(True)
         original.setVisible(False)
         duplicate.setVisible(True)
+
+        document.refreshProjection()
+
+    @staticmethod
+    def remove_hidden_layers() -> None:
+        """
+        Remove all hidden layers in the group of active layer.
+
+        - Hidden layers inside the subgroups are also removed.         
+        - Running the script when active layer is not inside any group,
+          results in removing all hidden layers in the document.
+        """
+        document = Krita.instance().activeDocument()
+        parent = document.activeNode().parentNode()
+
+        # Start searching for the hidden layers in current scope
+        affected_groups: list[GroupLayer] = [parent]
+
+        def remove_in_group(group: GroupLayer) -> None:
+            for node in group.childNodes():
+                print(type(node))
+                if not node.visible():
+                    node.remove()
+                elif isinstance(node, GroupLayer):
+                    # Visible group must be searched for hidden layers 
+                    affected_groups.append(node)
+
+        while affected_groups:
+            remove_in_group(affected_groups.pop())
 
         document.refreshProjection()
 
@@ -43,4 +72,5 @@ class WojtrybKritaHelpers(Extension):
             action.triggered.connect(callback)
         
         create_action("Backup Layer", self.backup_layer)
+        create_action("Remove Hidden Layers", self.remove_hidden_layers)
 
