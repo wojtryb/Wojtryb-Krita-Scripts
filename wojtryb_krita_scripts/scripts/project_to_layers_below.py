556e3ea7ab752cecadb5ecb94d00a5c6af03ee5e
Wojciech Trybus <wojtryb@gmail.com>
2025-08-09 12:30:31 +0200

    Add finding correct nodes to affect with "Project To Layers Below"

--- wojtryb_krita_scripts/scripts/project_to_layers_below.py
+++ wojtryb_krita_scripts/scripts/project_to_layers_below.py
@@ -1,31 +1,61 @@
 # SPDX-FileCopyrightText: © 2025 Wojciech Trybus <wojtryb@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from krita import Krita, Node
+from krita import Krita, Node, GroupLayer
+
+
+def _find_root(active_node: Node) -> Node:
+    root = active_node.parentNode()
+    while isinstance(root, GroupLayer) and root.passThroughMode():
+        root = root.parentNode()
+    return root
+
+
+def _find_nodes(active_node: Node, root: Node) -> list[Node]:
+    def walk(root: Node) -> tuple[list[Node], bool]:
+        output: list[Node] = []
+        for node in root.childNodes():
+            if node == active_node:
+                return output, True
+            if type(node) is Node:  # Paint layer, not its subclass
+                output.append(node)
+            elif isinstance(node, GroupLayer):
+                nodes, is_end = walk(node)
+                output.extend(nodes)
+                if is_end:
+                    return output, True
+        return output, False
+
+    nodes, _ = walk(root)
+    return nodes
 
 
 def project_to_layers_below() -> None:
     document = Krita.instance().activeDocument()
-    root = document.activeNode().parentNode()
-    nodes = root.childNodes()
-    current_node = nodes[-1]
+    active_node = document.activeNode()
+
+    root = _find_root(active_node)
+    nodes = _find_nodes(active_node, root)
 
     to_merge: list[Node] = []
 
     def duplicate_projection_above(node: Node):
-        copy = current_node.duplicate()
-        to_merge.append(copy)
-        root.addChildNode(copy, node)
+        if not node.locked():
+            copy = active_node.duplicate()
+            copy.setInheritAlpha(True)
+            to_merge.append(copy)
+            node.parentNode().addChildNode(copy, node)
 
         document.setActiveNode(node)
-        Krita.instance().action('selectopaque_add').trigger()
-        document.setActiveNode(current_node)
+        Krita.instance().action('selectopaque').trigger()
+        document.setActiveNode(active_node)
         Krita.instance().action('clear').trigger()
         Krita.instance().action('deselect').trigger()
 
     Krita.instance().action('deselect').trigger()
-    for i in range(2):
-        duplicate_projection_above(nodes[-2-i])
+
+    for node in reversed(nodes):
+        duplicate_projection_above(node)
 
     for node in to_merge:
         node.mergeDown()
