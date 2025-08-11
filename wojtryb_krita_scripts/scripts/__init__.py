4083d7aadfe0591215d5de44297f6b91d0b47a9a
Wojciech Trybus <wojtryb@gmail.com>
2025-08-11 11:11:00 +0200

    Add script "Flatten Inherit Alpha"

--- wojtryb_krita_scripts/scripts/__init__.py
+++ wojtryb_krita_scripts/scripts/__init__.py
@@ -4,6 +4,7 @@
 from .backup_layer import backup_layer
 from .change_collapse_state import change_collapse_state
 from .collapse_all_groups import collapse_all_groups
+from .flatten_inherit_alpha import flatten_inherit_alpha
 from .project_to_layers_below import project_to_layers_below
 from .remove_hidden_layers import remove_hidden_layers
 
@@ -11,5 +12,6 @@ __all__ = [
     "backup_layer",
     "change_collapse_state",
     "collapse_all_groups",
+    "flatten_inherit_alpha",
     "project_to_layers_below",
     "remove_hidden_layers"]
